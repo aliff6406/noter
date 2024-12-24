2003620fc0533809f85c273f693647b6d071eaef
Aliff Amir <aliff6406@gmail.com>
2024-12-24 20:43:26 +0000

    style: landing page styling

--- client/src/app/(marketing)/_components/heading.tsx
+++ client/src/app/(marketing)/_components/heading.tsx
@@ -1,10 +1,10 @@
 const heading = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center text-center">
       <h1 className="mb-5 text-7xl font-semibold">
         Welcome to <span className="underline">Noter</span>
       </h1>
-      <h3 className="text-2xl font-medium">
+      <h3 className="max-w-2xl text-2xl font-medium">
         The connected workspace where faster and better work gets done. Write. Brainstorm.
         Collaborate. With a little help from AI.
       </h3>
